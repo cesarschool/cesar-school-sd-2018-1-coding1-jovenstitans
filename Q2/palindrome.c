a7e160b547ea96e309b9c3945be87c95b83bd3dc
Carlos Tomaz <40301809+carlostomaz@users.noreply.github.com>
2018-06-18 23:11:17 -0300

    Update palindrome.c

--- Q2/palindrome.c
+++ Q2/palindrome.c
@@ -2,36 +2,42 @@
 #include <stdbool.h>
 
 /**
- * Escreva uma função que analisa uma dada palavra ou frase, retornando se a mesma é um palíndromo ou não.
- * 
- * Função: bool isPalindrome (char[] sentence, int length)
- * Input:
- * - sentence: array de caracteres em minúsculo da palavra ou frase a ser analisada.
- * - length: tamanho do array - número de caracteres
- * Output: true se a sentença é um palídromo, falso caso negativo.
- */
+* Escreva uma função que analisa uma dada palavra ou frase, retornando se a mesma é um palíndromo ou não.
+*
+* Função: bool isPalindrome (char[] sentence, int length)
+* Input:
+* - sentence: array de caracteres em minúsculo da palavra ou frase a ser analisada.
+* - length: tamanho do array - número de caracteres
+* Output: true se a sentença é um palídromo, falso caso negativo.
+*/
 
 bool isPalindrome (char sentence[], int length) {
-    printf("\nisPalindrome::START: %s, %d\n", sentence, length);
+   printf("\nisPalindrome::START: %s, %d\n", sentence, length);
 
-    // TODO: YOUR CODE HERE!
-    
-    printf("\nisPalindrome::END\n");
-    return false;
-}
+   // TODO: YOUR CODE HERE!
+   for(int i = 0; i < length; i++){
+       if (sentence[i] != sentence[length - 1 - i]){
+           printf("false");
+           printf("\nisPalindrome::END\n");
+           return false;
+       }// end if
+   }// end for loop
+   printf("true");
+   printf("\nisPalindrome::END\n");
+   return true;
+}// end isPalindrome
 
 int main()
 {
-    printf("\n### CESAR School :: Sistemas Digitais :: Coding1 :: Palindrome ###\n");
-    
-    // true cases
-    isPalindrome("ovo", 3);
-    isPalindrome("arara", 5);
+   printf("\n### CESAR School :: Sistemas Digitais :: Coding1 :: Palindrome ###\n");
+   
+   // true cases
+   isPalindrome("oso", 3);
+   isPalindrome("ralar", 5);
 
-    // false cases
-    isPalindrome("cesar", 5);
-    isPalindrome("school", 6);
+   // false cases
+   isPalindrome("cesar", 5);
+   isPalindrome("school", 6);
 
-    return 0;
+   return 0;
 }
- 
